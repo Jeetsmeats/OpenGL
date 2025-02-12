7200c74e97933b1b17fe097a8a532eddeef1edcb
Jeetsmeats <gunjeet.singh2@gmail.com>
2025-02-12 15:55:09 +1100

    bug with finding container image for texture test

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,20 @@
  * @Author: Gunjeet Singh
  * @Date:   2025-01-29 18:39:11
  * @Last Modified by:   Your name
- * @Last Modified time: 2025-02-12 11:54:01
+ * @Last Modified time: 2025-02-12 15:53:59
  */
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// include member functions/classes
 #include "callbacks.h"
 #include "key_input.h"
 #include "shader.h"
+
+// include images
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 using namespace std;
 
@@ -58,57 +63,101 @@ int main(int argc, char **argv)
     Shader ourShader("shaders/shader.vs", "shaders/shader.fs");
     ourShader.use();                        // activate program
 
+    // vertex coords
     float vertices[] = {
-        // First triangle
-        -0.5f,  -0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 
-        0.5f, -0.5f,  0.0f, 0.0f, 1.0f, 0.0f, 
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+        0.5f,-0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+        -0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f // top left
     };
 
+    // texture coords
+    float textCoords[] = {
+        0.0f, 0.0f,
+        1.0f, 0.0f, 
+        0.5f, 1.0f
+    };
+    
     // triangle indices
     unsigned int indices[] = {
-        0, 1, 2
+        0, 1, 3,
+        1, 2, 3
     };
-    
-    // Vertex buffer and and element buffer
-    unsigned int VBO;
-    // Vertex array object
-    unsigned int VAO;
 
-    // generate vertex buffer
-    glGenBuffers(1, &VBO);
-    
+    // Vertex buffer, array object and element buffer
+    unsigned int VBO, VAO, EBO;
+
     // generate vertex array object
     glGenVertexArrays(1, &VAO);
+    // generate vertex buffer
+    glGenBuffers(1, &VBO);
+    // generate element buffer
+    glGenBuffers(1, &EBO);
 
     // bind vertex array
     glBindVertexArray(VAO);
     
     // copy vertices into buffers
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // copy data into buffer
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // vertex attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) 0);
-    // set vertex attribute location
-    glEnableVertexAttribArray(0);
-    // vertex attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
-    // set vertex attribute location
-    glEnableVertexAttribArray(1);
-    
-    glBindVertexArray(0);
+    // copy vertices into buffers
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // copy data into buffer
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+    // define vertex attribute - position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) 0);
+    glEnableVertexAttribArray(0);
+    // define vertex attribute - color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // define vertex attributes - texture coordinates
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    
+    // define & generate textures
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    
+    // bind texture
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // set texture wrapping/filtering options
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    // define texture parameters
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        // generate texture w/ image
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        
+    } else 
+    {
+        cout << "Failed to load the texture" << endl;
+    }
+    
+    // free image memory
+    stbi_image_free(data);
+    
     // Wireframe Mode
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);  
     // Default Mode
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-    int nrAttributes;
-    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-    cout << "Maximum supported vertex attributes: " << nrAttributes << endl;
-
-    ourShader.use();
+    // check number of attributes
+    // int nrAttributes;
+    // glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    // cout << "Maximum supported vertex attributes: " << nrAttributes << endl;
+    
     while (!glfwWindowShouldClose(window)) 
     {
         processInput(window);           // key inputs
@@ -117,9 +166,13 @@ int main(int argc, char **argv)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw triangle
+        // bind texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // render container
+        ourShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);        // swap buffers
         glfwPollEvents();               // poll for events
@@ -127,6 +180,7 @@ int main(int argc, char **argv)
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     ourShader.deleteShader();
 
     glfwTerminate();
