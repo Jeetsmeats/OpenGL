c0e7701da57dad8414e08e0baeaf63e7b47d39ef
Jeetsmeats <gunjeet.singh2@gmail.com>
2025-02-13 18:38:27 +1100

    Container texture disco colours

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -9,5 +9,5 @@ uniform sampler2D texture1;
 // uniform sampler2D texture2;
 void main()
 {
-    FragColor = texture(texture1, TexCoord);
+    FragColor = texture(texture1, TexCoord) * vec4(ourColor, 1.0);
 }
