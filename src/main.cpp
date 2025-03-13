c2e5ee497e4cfd8fee31c2c09c8a5ea51a3e9c67
Jeetsmeats <gunjeet.singh2@gmail.com>
2025-03-14 00:28:07 +1100

    many containers

--- src/main.cpp
+++ src/main.cpp
@@ -113,11 +113,18 @@ int main(int argc, char **argv)
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
-    // transformation   
-    glm::mat4 model = glm::mat4(1.0f);                          // world space
-    glm::mat4 view = glm::mat4(1.0f);                           // view space
-    glm::mat4 projection = glm::mat4(1.0f);                     // projection space
-
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
     
     // Vertex buffer, array object and element buffer
     unsigned int VBO, VAO;
@@ -210,6 +217,9 @@ int main(int argc, char **argv)
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     glEnable(GL_DEPTH_TEST);
 
+    glm::mat4 view = glm::translate(view, glm::vec3(0.0f, 0.0f, -10.0f));
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) SCREEN_WIDTH / SCREEN_HEIGHT, 0.1f, 100.0f);
+
     // check number of attributes
     // int nrAttributes;
     // glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
@@ -229,25 +239,33 @@ int main(int argc, char **argv)
         glActiveTexture(GL_TEXTURE1); // texture unit 1
         glBindTexture(GL_TEXTURE_2D, texture2);
         
-        model = glm::rotate(model, (float) glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -10.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float) SCREEN_WIDTH / SCREEN_HEIGHT, 0.1f, 100.0f);
-        
-        // transformation matrix uniform locations
-        shader.setMat4("model", model);
-        shader.setMat4("view", view);
-        shader.setMat4("projection", projection);
-
-        // reinitialise buffer
-        model = glm::mat4(1.0f);                          // world space
         view = glm::mat4(1.0f);                           // view space
         projection = glm::mat4(1.0f);                     // projection space
 
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
+        projection = glm::perspective(glm::radians(45.0f),
+                 (float) SCREEN_HEIGHT / (float) SCREEN_WIDTH, 0.1f, 100.0f);
+
+        shader.setMat4("view", view);
+        shader.setMat4("projection", projection);
+
+        for (unsigned int i = 0; i < 10; i++) {
+            // transformation   
+            glm::mat4 model = glm::mat4(1.0f);                          // world space
+
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            // transformation matrix uniform locations
+            shader.setMat4("model", model);
+
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         // render container
         shader.use();
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
+    
         glfwSwapBuffers(window); // swap buffers
         glfwPollEvents();        // poll for events
     }
