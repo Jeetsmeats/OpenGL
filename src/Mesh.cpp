ebf43c857969ba15a803f4fef61f13b40393441f
Jeet Singh <gunjeet.singh2@gmail.com>
2026-05-25 22:29:04 +1000

    update with mesh

---
+++ src/Mesh.cpp
@@ -0,0 +1,38 @@
+#include "Mesh.h"
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures) {
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
+
+	setupMesh();
+}
+
+void Mesh::setupMesh() {
+
+	glGenVertexArrays(1, &this->_VAO);
+	glGenBuffers(1, &this->_VBO);
+	glGenBuffers(1, &this->_EBO);
+
+	glBindVertexArray(this->_VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, this->_VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, this->vertices.size() * sizeof(Vertex), &this->vertices[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->_EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices.size() * sizeof(unsigned int), &this->indices[0], GL_STATIC_DRAW);
+
+	// vertex positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+
+	// vertex normals
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, Normal));
+
+	// vertex texture coords
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, TexCoords));
+
+	glBindVertexArray(0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -306,29 +306,3 @@ void mouseCallback(GLFWwindow *window, double xpos, double ypos) {
 void scrollCallback(GLFWwindow *window, double xoffset, double yoffset) {
     camera.scrollCallback(window, xoffset, yoffset);
 }
-
-unsigned int loadTexture(const std::string &path) {
-    unsigned int textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
-    if (data) {
-        GLenum format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        stbi_image_free(data);
-    } else {
-        std::cout << "Failed to load texture: " << path << " - " << stbi_failure_reason() << std::endl;
-        glDeleteTextures(1, &textureID);
-        return 0;
-    }
-
-    return textureID;
-}
