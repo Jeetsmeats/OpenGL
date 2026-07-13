5972aa47b00a571c6b8ddc00c204b83c651cc35c
Gunjeet Singh <gunjeet.singh2@gmail.com>
2026-07-13 20:38:25 +1000

    add model

---
+++ src/Model.cpp
@@ -0,0 +1,100 @@
+#include "Model.h"
+
+#include <assimp/material.h>
+#include <iostream>
+
+Model::Model(char *path) {
+    loadModel(path);
+}
+
+void Model::loadModel(std::string path) {
+
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+        std::cout << "ERROR:ASSIMP::" << importer.GetErrorString() << std::endl;
+        return;
+    }
+    this->_dir = path.substr(0, path.find_last_of('/'));
+
+    processNode(scene->mRootNode, scene);
+}
+
+void Model::Draw(Shader &shader) {
+    for (unsigned int i = 0; i < _meshes.size(); i++) {
+        _meshes[i].Draw(shader);
+    }
+}
+
+void Model::processNode(aiNode *node, const aiScene *scene) {
+
+    // process all node's meshes
+    for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+        aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+        _meshes.push_back(processMesh(mesh, scene));
+    }
+
+    // recursively process all node's children
+    for (unsigned int i = 0; i < node->mNumChildren; i++) {
+        processNode(node->mChildren[i], scene);
+    }
+}
+
+Mesh Model::processMesh(aiMesh *mesh, const aiScene *scene) {
+
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    for (unsigned int i = 0; i < mesh->mNumVertices; i++) {                 // process vertices
+
+        Vertex vertex;
+        vertex.Position.x = mesh->mVertices[i].x;
+        vertex.Position.y = mesh->mVertices[i].y;
+        vertex.Position.z = mesh->mVertices[i].z;
+        vertex.Normal.x = mesh->mNormals[i].x;
+        vertex.Normal.y = mesh->mNormals[i].y;
+        vertex.Normal.z = mesh->mNormals[i].z;
+
+        if (mesh->mTextureCoords[0]) {
+            vertex.TexCoords.x = mesh->mTextureCoords[0][i].x;
+            vertex.TexCoords.y = mesh->mTextureCoords[0][i].y;
+        } else {
+            vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+        }
+
+        vertices.push_back(vertex);
+    }
+
+    for (unsigned int i = 0; i < mesh->mNumFaces; i++) {                    // process indices
+        aiFace face = mesh->mFaces[i];
+        for (unsigned int j = 0; j < face.mNumIndices; j++) {
+            indices.push_back(face.mIndices[j]);
+        }
+    }
+
+    if (mesh->mMaterialIndex >= 0) {                                        // process textures
+        aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
+        std::vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+        textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+    }
+    return Mesh(vertices, indices, textures);
+}
+
+std::vector<Texture> Model::loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName) {
+    std::vector<Texture> textures;
+
+    for (unsigned int i = 0; i < mat->GetTextureCount(type); i++) {
+        aiString str;
+        mat->GetTexture(type, i, &str);
+
+        Texture texture;
+        texture.id = TextureFromFile(str.C_Str(), _dir);
+        texture.type = typeName;
+        texture.path = str;
+
+        textures.push_back(texture);
+    }
+    return textures;
+}
